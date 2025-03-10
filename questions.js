ad4ac711c362fb4f05d99925cbf5399841752690
jessicabarrosz <jessbarros56@gmail.com>
2025-03-09 22:06:05 -0300

    arquivos do site giardíase

---
+++ questions.js
@@ -0,0 +1,117 @@
+let questions = [
+    {
+        numb: 1, 
+        question: "Qual é a principal forma infectante do Giardia?",
+        answer: " b) Cisto, com parede externa", 
+        options: [
+            "a) Trofozoíto em forma de esporo",
+            "b) Cisto, com parede externa",
+            "c) Esporozoíto",
+            "d) Cisto, sem parede externa"
+
+
+        ]
+    },
+    {
+        numb: 2, 
+        question: "Onde ocorre a transformação do cisto em trofozoíto?",
+        answer: "c) No duodeno e jejuno, por ação enzimática", 
+        options: [
+            "a) No estômago, no ambiente ácido",
+            "b) No intestino grosso, por ação bacteriana",
+            "c) No duodeno e jejuno, por ação enzimática", 
+            "d) No fígado, após a passagem pelo estômago"
+        ]
+    },
+    {
+        numb: 3, 
+        question: "Qual é a via de transmissão principal da giardíase?",
+        answer: " c) Via fecal-oral", 
+        options: [
+            "a) Exposição ao ar",
+            "b) Contato direto com a pele",
+            "c) Via fecal-oral",
+            "d) Transmissão através de insetos"
+        ]
+    },
+    {
+        numb: 4, 
+        question: "Quais são os sintomas típicos da giardíase aguda?",
+        answer: " b) Diarreia aquosa, dor abdominal e perda de apetite", 
+        options: [
+            "a) Diarreia com sangue e febre",
+            "b) Diarreia aquosa, dor abdominal e perda de apetite",
+            "c) Vômito e erupções cutâneas",
+            "d) Fadiga extrema e hemorragias internas"
+        ]
+    },
+    {
+        numb: 5, 
+        question: "Quanto tempo após a infecção os sintomas da giardíase aguda geralmente se manifestam?",
+        answer: "c) 1 a 14 dias, com variação individual", 
+        options: [
+            "a) 1 a 3 dias, de forma imediata",
+            "b) 1 a 7 dias, com sinais sutis",
+            "c) 1 a 14 dias, com variação individual",
+            "d) 7 a 21 dias, após sintomas iniciais"
+        ]
+    },
+    {
+        numb: 6, 
+        question: "Qual é o principal método diagnóstico para a giardíase",
+        answer: "a) Exame microscópico das fezes e pesquisa de antígenos", 
+        options: [
+            "a) Exame microscópico das fezes e pesquisa de antígenos",
+            "b) Exame de urina com teste rápido",
+            "c) Avaliação clínica com exames de imagem",
+            "d) Teste sorológico para detecção de anticorpos"
+        ]
+    },
+    {
+        numb: 7, 
+        question: "Qual é o tratamento padrão para giardíase?",
+        answer: "d) Tinidazol, metronidazol ou secnidazol", 
+        options: [
+            "a) Amoxicilina, ciprofloxacino ou azitromicina",
+            "b) Repouso e dieta sem lactose",
+            "c) Prednisona e oseltamivir",
+           "d) Tinidazol, metronidazol ou secnidazol"
+           
+        ]
+    },
+    {
+        numb: 8, 
+        question: "Qual é a forma ativa do Giardia responsável pelas manifestações clínicas?",
+        answer: "b) Trofozoíto, que adere à mucosa intestinal", 
+        options: [
+            "a) Esporozoíto que invade os tecidos",
+            "b) Trofozoíto, que adere à mucosa intestinal",
+            "c) Cisto, que não provoca sintomas",
+            "d) Toxina que circula no sangue"
+        ]
+    },
+    {
+        numb: 9, 
+        question: "Quais são os principais fatores de risco para a giardíase em países em desenvolvimento?",
+        answer: "b) Falta de saneamento básico e água potável", 
+        options: [
+            
+            "a) Dieta excessiva em carboidratos",
+            "b) Falta de saneamento básico e água potável",
+            "c) Uso de antibióticos indiscriminados",
+            "d) Exposição ao calor intenso e radiação solar"
+        ]
+    },
+    {
+        numb: 10, 
+        question: "Quais são as possíveis complicações da giardíase crônica em crianças?",
+        answer: "d) Má evolução ponderal e problemas de crescimento", 
+        options: [
+            "a) Alterações cognitivas e neurológicas",
+            "b) Sangramentos intestinais e desidratação grave",
+            "c) Atraso na resposta imunológica e febre contínua",
+            "d) Má evolução ponderal e problemas de crescimento"
+           
+        ]
+    },
+];
